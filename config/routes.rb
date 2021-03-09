56786fc8288552b2ac3052c7761b40a7a128a588
Frederik Fabricius Smitt <frederikfabriciussmitt@Frederiks-MacBook-Pro.local>
2021-03-09 09:12:06 +0100

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  #Jeg excepter show, fordi jeg ikke vil have portfolios som flertal
+  get 'portfolio/:id',  to: 'portfolios#show',  as: 'portfolio_show'
+  #Dette betyder, at jeg f.eks. skal sætte portfolio_show_path i mine views, hvis jeg skal linke til et portfolio
 
   get 'about-me', to: 'pages#about'
   get 'contact', to:'pages#contact'
