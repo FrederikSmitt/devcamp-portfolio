6e045d303a51c9462ffaf4c043661c266c7d6e3e
Frederik Fabricius Smitt <frederikfabriciussmitt@Frederiks-MacBook-Pro.local>
2021-03-11 09:29:59 +0100

    Updated status buttons in Blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+
   #Jeg excepter show, fordi jeg ikke vil have portfolios som flertal
   get 'portfolio/:id',  to: 'portfolios#show',  as: 'portfolio_show'
   #Dette betyder, at jeg f.eks. skal sætte portfolio_show_path i mine views, hvis jeg skal linke til et portfolio
@@ -7,7 +8,11 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to:'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
